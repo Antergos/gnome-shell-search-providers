a7857b02523d482a4749fb29ef1bc60ca7d19a4f
Alex Filgueira <faidoc@gmail.com>
2017-01-10 16:33:29 -0500

    Upload tvshows search provider

---
+++ tvshows/api.js
@@ -0,0 +1,329 @@
+/* TheMovieDB Search Provider for Gnome Shell
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+const Lang = imports.lang;
+const Soup = imports.gi.Soup;
+const Params = imports.misc.params;
+
+const PROTOCOL = 'https';
+const BASE_URL = 'api.themoviedb.org';
+const API_VERSION = '3';
+const API_KEY = '668b45ee89c507efc43cc8eda2f7290c';
+const API_PATH = 'search/tv';
+const API_POSTER_PATH = 'http://image.tmdb.org/t/p/';
+const API_POSTER_DEFAULT_SIZE = 'w185';
+const DEFAULT_LANG = 'en';
+const LIMIT_VIEW = '10';
+const HTTP_TIMEOUT = 10;
+const USER_AGENT = 'TheMovieDBSearchProvider extension for GNOME Shell by Antergos';
+
+const IMDB_URL = 'http://www.imdb.com/title/';
+const PROVIDER_URL = 'https://www.themoviedb.org/';
+
+/**
+ * Client that interacts with TheMovieDB API
+ *
+ * @class Api
+ * @uses imports.gi.Soup
+ * @uses imports.misc.params
+ */
+const Api = new Lang.Class({
+    Name: 'Api',
+
+    /**
+     * Set default parameters and create a Soup session.
+     * @constructor
+     * @param {Object} params Parameters
+     * @private
+     */
+    _init: function(params) {
+        /**
+         * @property {Object} _params
+         * @private
+         */
+        this._params = Params.parse(params, {
+            /**
+             * @property {String} _params.protocol=PROTOCOL API protocol
+             * @accessor
+             */
+            protocol: PROTOCOL,
+            /**
+             * @property {String} _params.baseUrl=BASE_URL API base url
+             */
+            baseUrl: BASE_URL,
+            /**
+             * @property {String} _params.apiVersion=API_VERSION API version
+             */
+            apiVersion: API_VERSION,
+            /**
+             * @property {String} _params.apiKey=API_KEY API key
+             */
+            apiKey: API_KEY,
+            /**
+             * @property {String} _params.apiPath=API_PATH API path
+             */
+            apiPath: API_PATH,
+            /**
+             * @property {String} _params.apiPosterPath=API_POSTER_PATH API path for images
+             */
+            apiPosterPath: API_POSTER_PATH,
+            /**
+             * @property {String} _params.apiPosterSize=API_POSTER_DEFAULT_SIZE API poster default size
+             */
+            apiPosterSize: API_POSTER_DEFAULT_SIZE,
+            /**
+             * @property {String} _params.language=DEFAULT_LANG API language
+             * @accessor
+             */
+            language: DEFAULT_LANG,
+            /**
+             * @property {String} _params.limit=LIMIT_VIEW API result limit to show
+             */
+            limit: LIMIT_VIEW
+        });
+
+        /**
+         * @property {Soup.SessionAsync} _session Soup session
+         * @private
+         */
+        this._session = new Soup.SessionAsync();
+        Soup.Session.prototype.add_feature.call(
+            this._session,
+            new Soup.ProxyResolverDefault()
+        );
+        this._session.user_agent = USER_AGENT;
+        this._session.timeout = HTTP_TIMEOUT;
+    },
+
+    /**
+     * Construct the API URL
+     * @returns {String} Language specific API URL that expects a response
+     * in JSON
+     * @private
+     */
+    _getApiUrl: function() {
+        return '%s://%s/%s/%s?api_key=%s&language=%s'
+            .format(PROTOCOL, BASE_URL, API_VERSION, this.apiPath, API_KEY, this.language);
+    },
+
+    /**
+     * Construct query URL using the API URL and query parameters
+     * @param {Object} queryParameters
+     * @returns {String} Query URL
+     * @private
+     */
+    _getQueryUrl: function(queryParameters) {
+        let queryString = '',
+            url = this._getApiUrl(),
+            parameter;
+
+        for(parameter in queryParameters) {
+            if(queryParameters.hasOwnProperty(parameter)) {
+                queryString += '&%s=%s'.format(
+                    parameter,
+                    encodeURIComponent(queryParameters[parameter])
+                )
+            }
+        }
+
+        url += queryString;
+
+        // Final URL to be queried
+        log(url);
+
+        return url;
+    },
+
+    /**
+     * Query the API
+     * @param {Object} queryParameters Query parameters
+     * @param {Function} callback Callback that will be called with an
+     * error message or a result.
+     * @param {null|String} callback.errorMessage Message describing
+     * what went wrong
+     * @param {Object|null} callback.result Response data parsed in JSON format
+     */
+    get: function(queryParameters, callback) {
+        let queryUrl = this._getQueryUrl(queryParameters),
+            request = Soup.Message.new('GET', queryUrl),
+            result;
+
+        this._session.queue_message(request,
+            Lang.bind(this, function(http_session, message) {
+                let errorMessage;
+
+                if(message.status_code !== Soup.KnownStatusCode.OK) {
+                    errorMessage = "Api.Client.get: Error code: %s"
+                        .format(message.status_code);
+                    log(errorMessage);
+                    callback(errorMessage, null);
+                } else {
+                    try {
+                        result = JSON.parse(request.response_body.data);
+                        callback(null, result);
+                    } catch(e) {
+                        errorMessage = "Api.Client.get: %s".format(e);
+                        log('%s. Response body: %s'
+                            .format(errorMessage, request.response_body.data)
+                        );
+                        callback(errorMessage, null);
+                    }
+                }
+            })
+        );
+    },
+
+
+    /**
+     * Query the API for item detail
+     * @param {int} id Item ID
+     * @param {Function} callback Callback that will be called with an
+     * error message or a result.
+     * @param {null|String} callback.errorMessage Message describing
+     * what went wrong
+     * @param {Object|null} callback.result Response data parsed in JSON format
+     */
+    getDetail: function(id, callback) {
+        let queryUrl = this._getQueryUrl(''),
+            request = Soup.Message.new('GET', queryUrl),
+            result;
+
+        this._session.queue_message(request,
+            Lang.bind(this, function(http_session, message) {
+                let errorMessage;
+
+                if(message.status_code !== Soup.KnownStatusCode.OK) {
+                    errorMessage = "Api.Client.get: Error code: %s"
+                        .format(message.status_code);
+                    log(errorMessage);
+                    callback(errorMessage, null);
+                } else {
+                    try {
+                        result = JSON.parse(request.response_body.data);
+                        callback(null, result);
+                    } catch(e) {
+                        errorMessage = "Api.Client.get: %s".format(e);
+                        log('%s. Response body: %s'
+                            .format(errorMessage, request.response_body.data)
+                        );
+                        callback(errorMessage, null);
+                    }
+                }
+            })
+        );
+
+        // Reset the API PATH to default value for following requests
+        this.apiPath = API_PATH;
+    },
+
+    /**
+     * Search entities
+     *
+     * @param {String} term Query to search for
+     * @param {Function} callback Callback that will be called with an
+     * error message or a result.
+     * @param {null|String} callback.errorMessage Message describing
+     * what went wrong
+     * @param {Object|null} callback.result Response data parsed in JSON format
+     * @param {Number} [continue_=0] Get results starting at this index
+     */
+    searchEntities: function (term, callback, continue_) {
+        continue_ = continue_ || 0;
+        this.get({
+            query: term,
+            include_adult: '0'
+        }, callback);
+    },
+
+    searchItemDetails: function (id, callback, continue_) {
+        // Set API Path to search for movie details
+        this.apiPath = 'tv/' + id + '/external_ids';
+
+        continue_ = continue_ || 0;
+        this.getDetail(id, callback);
+    },
+
+    /**
+     * Delete the Soup session
+     */
+    destroy: function() {
+        this._session.run_dispose();
+        this._session = null;
+    },
+
+    /**
+     * Get the API protocol
+     * @method getProtocol
+     * @returns {String} this._params.protocol
+     */
+    get protocol() {
+        return this._params.protocol;
+    },
+
+    get apiPath() {
+        return this._params.apiPath;
+    },
+    set apiPath(path) {
+        this._params.apiPath = path;
+    },
+
+    /**
+     * Get the API language
+     * @method getLanguage
+     * @returns {String} this._params.language
+     */
+    get language() {
+        return this._params.language;
+    },
+
+    /**
+     * Set the API language
+     * @method getLanguage
+     * @param {String} language
+     */
+    set language(language) {
+        this._params.language = language;
+    },
+
+    /**
+     * Get the default API language
+     * @method getDefaultLanguage
+     * @returns {String} DEFAULT_LANG
+     */
+    get defaultLanguage() {
+        return DEFAULT_LANG;
+    },
+
+    /**
+     * Get the default API result limit
+     * @method getLimit()
+     * @returns {String} LIMIT_VIEW
+     */
+    get limit() {
+        return LIMIT_VIEW;
+    },
+
+    set apiPosterSize(size) {
+        this._params.apiPosterSize = size;
+    },
+    get apiPosterSize() {
+        return API_POSTER_DEFAULT_SIZE;
+    },
+    get apiPosterPath() {
+        return API_POSTER_PATH;
+    },
+    get imdbUrl() {
+        return IMDB_URL;
+    },
+    get providerUrl() {
+        return PROVIDER_URL;
+    }
+
+});
---
+++ tvshows/extension.js
@@ -0,0 +1,379 @@
+/* TheMovieDB Search Provider for TvShows for Gnome Shell
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ */
+
+const Main = imports.ui.main;
+const Mainloop = imports.mainloop;
+const Gio = imports.gi.Gio;
+const Lang = imports.lang;
+const Util = imports.misc.util;
+const Soup = imports.gi.Soup;
+const Glib = imports.gi.GLib;
+const St = imports.gi.St;
+
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Api = Me.imports.api;
+
+const Gettext = imports.gettext.domain('themoviedb_tvshows_search_provider');
+const _ = Gettext.gettext;
+
+const CACHE_FOLDER = '.cache/antergos-search-providers-gnome/tvshows';
+
+const TheMovieDBTvShowsSearchProvider = new Lang.Class({
+    Name: 'TheMovieDBTvShowsSearchProvider',
+
+    _init: function() {
+        let self = this;
+
+        // Create cache
+        let cache_folder = Gio.file_new_for_path(CACHE_FOLDER);
+        if (!cache_folder.query_exists(null)) {
+            cache_folder.make_directory(null);
+        }
+
+
+        // Use the default app for opening https links as the app for
+        // launching full search.
+        this.appInfo = Gio.AppInfo.get_default_for_uri_scheme('https');
+        // Fake the name and icon of the app
+        this.appInfo.get_name = function() {
+            return _('TheMovieDB Search Provider for TvShows');
+        };
+        this.appInfo.get_icon = function() {
+            return Gio.icon_new_for_string(Me.path + "/icon.svg");
+        };
+
+        // Custom messages that will be shown as search results
+        this._messages = {
+            '__loading__': {
+                id: '__loading__',
+                name: 'TheMovieDB - TvShows',
+                description : _('Loading items from TheMovieDB, please wait...'),
+                createIcon: Lang.bind(this, this.createIcon, {})
+            },
+            '__error__': {
+                id: '__error__',
+                name: 'TheMovieDB - TvShows',
+                description : _('Oops, an error occurred while searching.'),
+                createIcon: Lang.bind(this, this.createIcon, {})
+            }
+        };
+        // API results will be stored here
+        this.resultsMap = new Map();
+        this._api = new Api.Api();
+		// Wait before making an API request
+		this._timeoutId = 0;
+
+
+        // Configure default language for results
+        this._current_language_code_large = Glib.getenv('LANG');
+        this._language = this._current_language_code_large.substr(0, 2);
+
+        // Set user's home PATH
+        this._home_path = Glib.getenv('HOME');
+    },
+
+
+    /**
+     * Search API if the query is a TheMovieDB query.
+     * TheMovieDB query must start with a 'wd' as the first term.
+     * @param {Array} terms
+     * @param {Function} callback
+     * @param {Gio.Cancellable} cancellable
+     */
+    getInitialResultSet: function(terms, callback, cancellable) {
+        let meta;
+
+        if (terms.length >= 2 && terms[0].substr(0, 2) === 'tv') {
+            // show the loading message
+            this.showMessage('__loading__', callback);
+            // remove previous timeout
+            if (this._timeoutId > 0) {
+                Mainloop.source_remove(this._timeoutId);
+                this._timeoutId = 0;
+            }
+
+            // wait 0.2 seconds before making an API request
+            this._timeoutId = Mainloop.timeout_add(1000, Lang.bind(this, function() {
+                // set the language
+                if (this._language){
+                    this._api.language = this._language;
+                } else {
+                    this._api.language = this._api.defaultLanguage;
+                }
+
+                // now search
+                this._api.searchEntities(
+                    this._getQuery(terms),
+                    Lang.bind(this, this._getResultSet, callback, this._timeoutId)
+                );
+            }));
+        } else {
+            // return an emtpy result set
+            this._getResultSet(null, {}, callback, 0);
+        }
+    },
+
+
+    /**
+     * @param {Array} previousResults
+     * @param {Array} terms
+     * @returns {Array}
+     */
+    getSubsetResultSearch: function (previousResults, terms, callback) {
+        let meta;
+
+        if (terms.length >= 2 && terms[0].substr(0, 2) === 'tv') {
+            // show the loading message
+            this.showMessage('__loading__', callback);
+            // remove previous timeout
+            if (this._timeoutId > 0) {
+                Mainloop.source_remove(this._timeoutId);
+                this._timeoutId = 0;
+            }
+
+            // wait 0.2 seconds before making an API request
+            this._timeoutId = Mainloop.timeout_add(1000, Lang.bind(this, function() {
+                // set the language
+                if (this._language){
+                    this._api.language = this._language;
+                } else {
+                    this._api.language = this._api.defaultLanguage;
+                }
+
+                // now search
+                this._api.searchEntities(
+                    this._getQuery(terms),
+                    Lang.bind(this, this._getResultSet, callback, this._timeoutId)
+                );
+            }));
+        } else {
+            // return an emtpy result set
+            this._getResultSet(null, {}, callback, 0);
+        }
+    },
+
+
+    /**
+     * Run callback with results
+     * @param {Array} identifiers
+     * @param {Function} callback
+     */
+    getResultMetas: function(identifiers, callback) {
+        let metas = [];
+        for (let i = 0; i < identifiers.length; i++) {
+            metas.push(this._getResultMeta(identifiers[i]));
+        }
+        callback(metas);
+    },
+
+
+    /**
+     * Return meta from result
+     * @param {String} identifier
+     * @returns {{id: String, name: String, description: String, createIcon: Function}}
+     * @private
+     */
+    _getResultMeta: function(identifier, callback) {
+        let result,
+            meta;
+
+        // return predefined message if it exists
+        if (identifier in this._messages) {
+            result = this._messages[identifier];
+        } else {
+            meta = this.resultsMap.get(identifier);
+
+            let apiPosterPath = this._api.apiPosterPath,
+                apiPosterSize = this._api.apiPosterSize;
+
+            let icon_path = meta.poster_path ? meta.poster_path : '',
+                icon_url = meta.poster_path ? apiPosterPath + '/' + apiPosterSize + '/' + meta.poster_path : '',
+                release_date = meta.first_air_date.split("-"),
+                release_date_beautify = release_date[0] ? ' - (' + release_date[0] + ')' : '',
+                vote_average = meta.vote_average >= 1 ? ' - ' + meta.vote_average : '';
+
+            result = {
+                id: meta.id,
+                name: meta.name + release_date_beautify + vote_average,
+                description : meta.overview,
+                createIcon: Lang.bind(this, this._cachePoster, icon_path, icon_url, callback)
+            };
+        }
+        return result;
+    },
+
+
+    /**
+     * Launch the search in the default app (i.e. browser)
+     */
+    launchSearch: function () {
+        Util.trySpawnCommandLine(
+            "xdg-open " + this._api.providerUrl);
+    },
+
+    /**
+     * Open the url in default app
+     * @param {String} identifier
+     * @param {Array} terms
+     * @param timestamp
+     * @param callback
+     */
+    activateResult: function(identifier, terms, timestamp, callback) {
+        // only do something if the result is not a custom message
+        if (!(identifier in this._messages)) {
+            item = this.resultsMap.get(identifier);
+
+            this._api.searchItemDetails(
+                item.id,
+                Lang.bind(this, this._openLink, callback, this._timeoutId)
+            );
+        }
+    },
+
+
+    /**
+     * Show any message as a search item
+     * @param {String} identifier Message identifier
+     * @param {Function} callback Callback that pushes the result to search
+     * overview
+     */
+    showMessage: function (identifier, callback) {
+        callback([identifier]);
+    },
+
+
+    /**
+     * Return subset of results
+     * @param {Array} results
+     * @param {number} max
+     * @returns {Array}
+     */
+    filterResults: function(results, max) {
+        // override max for now
+        max = this._api.limit;
+        return results.slice(0, max);
+    },
+
+
+
+    /**
+     * Return query string from terms array
+     * @param {String[]} terms
+     * @returns {String}
+     */
+    _getQuery: function(terms) {
+        return terms.slice(1).join(' ');
+    },
+
+
+    /**
+     * Parse results that we get from the API and save them in this.resultsMap.
+     * Inform the user if no results are found.
+     * @param {null|String} error
+     * @param {Object|null} result
+     * @param {Function} callback
+     * @param {Function} timeoutId
+     * @private
+     */
+    _getResultSet: function (error, result, callback, timeoutId) {
+        let self = this,
+            results = [];
+
+        if (timeoutId === this._timeoutId && result.results && result.results.length > 0) {
+            result.results.forEach(function (result) {
+                self.resultsMap.set(result.id, result);
+                results.push(result.id);
+            });
+            callback(results);
+        } else if (error) {
+            // Let the user know that an error has occurred.
+            log(error);
+            this.showMessage('__error__', callback);
+        } else {
+            callback(results);
+        }
+
+    },
+
+
+    _openLink: function(error, result) {
+        if (result) {
+            Util.trySpawnCommandLine(
+                "xdg-open " + this._api.imdbUrl + result.imdb_id + '/');
+        }
+    },
+
+
+    /**
+     * Create meta icon
+     * @param size
+     * @param {Object} meta
+     */
+    createIcon: function (size, meta) {
+        // TODO: implement meta icon?
+    },
+
+    _cachePoster: function (object, image_name, url, callback) {
+        let _httpSession = new Soup.SessionAsync();
+
+        if(url && typeof url === 'string' && image_name !== '') {
+            Soup.Session.prototype.add_feature.call(_httpSession, new Soup.ProxyResolverDefault());
+
+            // open the file
+            let file = Gio.file_new_for_path(this._home_path + '/' + CACHE_FOLDER + image_name);
+
+            if(!file.query_exists(null)) {
+                let fstream = file.replace(null, false, Gio.FileCreateFlags.NONE, null);
+
+                let request = Soup.Message.new('GET', url);
+                request.connect('got_chunk', Lang.bind(this, function(message, chunk){
+                    // write each chunk to file
+                    fstream.write(chunk.get_data(), null, chunk.length);
+                }));
+
+                _httpSession.queue_message(request, function(_httpSession, message) {
+                    // close the file
+                    fstream.close(null);
+                });
+                return new St.Icon({ gicon: Gio.icon_new_for_string(this._home_path + '/' + CACHE_FOLDER + image_name), style_class: 'themoviedb-search-provider-poster'});
+            }else {
+                log('File exists on cache for image name: %s'.format(image_name));
+
+                return new St.Icon({ gicon: Gio.icon_new_for_string(this._home_path + '/' + CACHE_FOLDER + image_name), style_class: 'themoviedb-search-provider-poster'});
+            }
+
+        }
+    }
+});
+
+let theMovieDBTvShowsSearchProvider = null;
+
+function init() {
+    /** noop */
+}
+
+function enable() {
+    if (!theMovieDBTvShowsSearchProvider) {
+        theMovieDBTvShowsSearchProvider = new TheMovieDBTvShowsSearchProvider();
+        Main.overview.viewSelector._searchResults._registerProvider(
+            theMovieDBTvShowsSearchProvider
+        );
+    }
+}
+
+function disable() {
+    if (theMovieDBTvShowsSearchProvider){
+        Main.overview.viewSelector._searchResults._unregisterProvider(
+            theMovieDBTvShowsSearchProvider
+        );
+        theMovieDBTvShowsSearchProvider = null;
+    }
+}
+
